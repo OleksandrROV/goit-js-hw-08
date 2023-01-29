a4b01e89b0d9969b8e56fd80f08425f03086d9b1
OleksandrRusnak <111389426+OleksandrROV@users.noreply.github.com>
2023-01-29 15:36:34 +0100

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+// Add imports above this
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+// Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+
+function createGallery(items) {
+  return items
+    .map(item => {
+      return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${item.original}">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                />
+                </a>
+            </div>`;
+    })
+    .join('');
+}
+galleryContainer.innerHTML = createGallery(galleryItems);
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const KEY_LOCALSTORAGE = 'feedback-form-state';
+
+const formObject = {
+  email: document.querySelector('input'),
+  message: document.querySelector('textarea'),
+  form: document.querySelector('form'),
+};
+
+const params = {};
+
+formObject.form.addEventListener('input', throttle(onFormInput, 500));
+
+formObject.form.addEventListener('submit', submitForm);
+
+function onFormInput(event) {
+  const key = event.target.name;
+  params[key] = event.target.value;
+  localStorage.setItem(KEY_LOCALSTORAGE, JSON.stringify(params));
+}
+
+function submitForm() {
+  event.preventDefault();
+  console.log(params);
+  formObject.form.reset();
+  localStorage.removeItem(KEY_LOCALSTORAGE);
+}
+function controlData() {
+  const data = localStorage.getItem(KEY_LOCALSTORAGE);
+  const parseData = JSON.parse(data);
+
+  if (parseData.email) {
+    formObject.email.value = parseData.email;
+  }
+  if (parseData.message) {
+    formObject.message.value = parseData.message;
+  }
+}
+controlData();
